bffc71d1488e38473407311da543609ae9504257
Jan Kaßel <jan.kassel@rwth-aachen.de>
2015-08-06 09:43:09 +0200

    Added redis persistency

--- bot.js
+++ bot.js
@@ -1,20 +1,49 @@
 var TelegramBot = require('node-telegram-bot-api'),
-  CronJob = require('cron').CronJob;
+  CronJob = require('cron').CronJob,
+  redis = require('redis');
 
 var token = process.env.TOKEN,
   bot = new TelegramBot(token, { polling: true }),
-  job = null;
+  client = redis.createClient(6379, 'chipmonkey'),
+  job = null,
+  chats = [];
 
-var chats = [];
+client.get('chats', function (err, reply) {
+  if (err)
+    throw err;
+
+  if (reply !== null) {
+    try {
+      data = JSON.parse(reply.toString());
+      
+      if (Arrays.isArray(data)) {
+        chats = data;
+        console.log('Restored state from redis (%s)',
+          JSON.stringify(chats));
+      }
+    }
+
+    catch (e) {
+      console.warn('Warning: Couldn\'t restore state from redis.');
+    }
+  }
+
+  else {
+    console.log('No restorable state found')
+  }
+});
+
+function sync () {
+  client.set('chats', JSON.stringify(chats));
+}
 
 bot.getMe().then(function (me) {
-  console.log('Hi, my name is %s!', me.username);
+  console.log('Hi, my name is %s %s. I\'m a bot!',
+    me.first_name, me.last_name);
 });
 
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
-
-  console.log('onMessage: ', msg.text);
 
   if (msg.text === '/start') {
     if (chats.indexOf(chatId) !== -1) {
@@ -22,25 +51,49 @@ bot.on('message', function (msg) {
     }
 
     else {
-      bot.sendMessage(chatId, 'Guten Tag, edle Logiker! Ich bin Kurt Gödel, und werde Ihren Lerntag um logische Fakten bereichern.');
+      bot.sendMessage(chatId, 'Guten Tag, edle Logiker! Ich bin Kurt Gödel, ' +
+        'und werde Ihren Lerntag um logische Fakten bereichern.');
       chats.push(chatId);
+      sync();
     }
   }
 
-  else if (msg.text === '/digest') {
-    bot.sendMessage(chatId, 'O, welch schöner Tag! Edle Logiker, begebt Euch in die Lernräume, um der Logik fröhnen!');
+  else if (msg.text === '/startsilent') {
+    if (chats.indexOf(chatId) === -1) {
+      chats.push(chatId);
+      sync();
+
+      console.log('Silent start');
+    }
+  }
+
+  else if (msg.text === '/hallo') {
+    bot.sendMessage(chatId, 'O, welch schöner Tag! Edle Logiker, begebt Euch ' +
+      'in die Lernräume, um der Logik zu fröhnen!');
+  }
+
+  else if (msg.text === '/tschuess') {
+    var index = chats.indexOf(chatId);
+    if (index > -1) {
+      chats.splice(index, 1);
+      sync();
+
+      bot.sendMessage(chatId, 'Logik steckt überall! Mich werdet Ihr niemals los!');
+    }
   }
 });
 
-job = new CronJob('00 00 09 * * *', function () {
-  chats.forEach(function (chatId) {
-    bot.sendMessage(chatId, 'O, welch schöner Tag! Edle Logiker, begebt Euch in die Lernräume, um der Logik fröhnen!');
-  })
-})
+job = new CronJob('00 00 10 * * *', function () {
+  console.log('Digest job called');
 
-job.start();
+  chats.forEach(function (chatId) {
+    bot.sendMessage(chatId, 'O, welch schöner Tag! Edle Logiker, begebt Euch' +
+      ' in die Lernräume, um der Logik zu fröhnen!');
+  })
+}, true, 'Europe/Berlin')
 console.log('Digest job started.');
 
 process.on('exit', function () {
   job.stop();
+  sync();
 });
