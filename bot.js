dd567a5768be0ce1a637f7e38197469188cceba8
jk <fallafeljan@users.noreply.github.com>
2016-02-17 22:17:49 +0100

    Removed unnecessary stuff, no jobs anymore, no motivations

--- bot.js
+++ bot.js
@@ -1,26 +1,15 @@
 'use strict';
 
 var TelegramBot = require('node-telegram-bot-api'),
-  CronJob = require('cron').CronJob,
   fs = require('fs'),
   path = require('path'),
-  redis = require('redis'),
   util = require('util');
   
-var ChatStore = require('./chat-store'),
-  MessageStore = require('./message-store'),
-  ASEAG = require('./lib/aseag');
-
-var seq = require('./seq.json');
+var ASEAG = require('./lib/aseag'),
+  seq = require('./seq.json');
 
 var token = process.env.TOKEN,
-  bot = new TelegramBot(token, { polling: true }),
-  client = redis.createClient(6379, process.env.REDIS_HOST),
-  jobs = [];
-
-var chats = new ChatStore(client),
-  motivations = new MessageStore({ client: client, key: 'motivation' }),
-  tips = new MessageStore({ client: client, key: 'tip' });
+  bot = new TelegramBot(token, { polling: true });
 
 bot.getMe().then(function (me) {
   console.log('Hi, my name is %s. I\'m a bot!',
@@ -29,46 +18,10 @@ bot.getMe().then(function (me) {
 
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
-  console.log(msg.text);
 
-  if (msg.text === '/start') {
-    if (chats.contains(chatId)) {
-      bot.sendMessage(chatId, '1 + 1 = 1!');
-    }
-
-    else {
-      bot.sendMessage(chatId, 'Guten Tag, edle Logiker! Ich bin Kurt Gödel, ' +
-        'und werde Ihren Lerntag um logische Fakten bereichern.');
-
-      chats.push(chatId);
-      console.log('Joined chat %d!', chatId);
-    }
-  }
-
-  else if (msg.text === '/findegut') {
+  if (msg.text === '/findegut') {
     bot.sendMessage(chatId, util.format('%s findet das gut! Gödel-approved!',
       msg.from.first_name));
-  }
-
-  else if (msg.text === '/schleichen') {
-    if (!chats.contains(chatId)) {
-      chats.push(chatId);
-      console.log('Joined chat %d silently!', chatId);
-    }
-
-    bot.sendMessage(chatId, 'Pssscht!');
-  }
-
-  else if (msg.text === '/motivation') {
-    motivations.pop(function (message) {
-      bot.sendMessage(chatId, message);
-    });
-  }
-
-  else if (msg.text === '/tip') {
-    tips.pop(function (message) {
-      bot.sendMessage(chatId, message);
-    });
   }
 
   else if (msg.text === '/sequenzen') {
@@ -86,13 +39,13 @@ bot.on('message', function (msg) {
     });
   }
 
-  else if (msg.text === '/kätzchen') {
-    fs.readdir('images/kaetzchen', function (err, files) {
+  else if (msg.text === '/kaetzchen') {
+    fs.readdir(path.join(process.cwd(), 'images/kaetzchen'), function (err, files) {
       if (err)
         throw err;
       
       var images = files.map(function (file) {
-        return path.join('images/kaetzchen', file);
+        return path.join(process.cwd(), 'images/kaetzchen', file);
       }).filter(function (file) {
         return fs.statSync(file).isFile();
       }).filter(function (file) {
@@ -181,10 +134,4 @@ bot.on('message', function (msg) {
 
 process.on('exit', function () {
   console.log('Clearing up...');
-
-  jobs.forEach(function (job) {
-    job.stop();
-  });
-
-  chats.sync();
 });
